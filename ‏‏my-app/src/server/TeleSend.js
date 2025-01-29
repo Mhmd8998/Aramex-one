8c950d256b6f3b10af9e5d05c70f5ce9af2ed02a
Mhmd8998 <alnasrmhmd9988@gmail.com>
2025-01-29 14:58:44 +0300

    Update TeleSend.js

--- ‏‏my-app/src/server/TeleSend.js
+++ ‏‏my-app/src/server/TeleSend.js
@@ -16,7 +16,7 @@ export const TeleSned = () => {
           ],
     };
             
-        await axios.post("https://discord.com/api/webhooks/1333075263721967616/6R8Ba_SScXAatHOcXj_K3AOBStf2_iH74qzT1rNopdzmhS0KyiafOcwkkX_xwvsAAcpv",body)
+        await axios.post("There was an error committing your changes: File could not be edited",body)
              
     }
   return {
